15c6326b9e8fd72e3628931574021c65320f9f8f
fridah mbithe <fridahmbithe45@gmail.com>
2024-05-28 15:46:11 +0300

    first assignment

---
+++ Assigment/user_input.py
@@ -0,0 +1,8 @@
+
+name = input("Enter your name:")
+
+age = input ("Enter your age:")
+location = input ("Enter your location:")
+
+#f-string allows one to include variables i.e name , location etc in a string
+print (f"Hello {name},you are {age} years old and live in {location}")
